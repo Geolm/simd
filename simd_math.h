4565910a68e8e5959a1ccfdae91e546b930004f1
Geolm <moyaguillaume@gmail.com>
2024-01-08 15:41:12 -0500

    moved big functions to simd_math.c

--- simd_math.h
+++ simd_math.h
@@ -8,6 +8,22 @@
 #define SIMD_MATH_PI2 (1.57079632f)
 
 //----------------------------------------------------------------------------------------------------------------------
+// prototypes
+static simd_vector simd_sin(simd_vector x);
+static simd_vector simd_acos(simd_vector x);
+static simd_vector simd_cos(simd_vector a);
+static simd_vector simd_atan2(simd_vector x, simd_vector y);
+       simd_vector simd_log(simd_vector x);
+static simd_vector simd_approx_cos(simd_vector a);
+static simd_vector simd_approx_sin(simd_vector a);
+static simd_vector simd_approx_exp(simd_vector x);
+static simd_vector simd_approx_pow(simd_vector x, simd_vector exponent, uint32_t degree);
+static simd_vector simd_approx_srgb_to_linear(simd_vector value);
+static simd_vector simd_approx_linear_to_srgb(simd_vector value);
+static simd_vector simd_smoothstep(simd_vector edge0, simd_vector edge1, simd_vector x);
+
+
+//----------------------------------------------------------------------------------------------------------------------
 // from hlslpp
 // max error with input [-PI; PI] : 0.000001
 static inline simd_vector simd_sin(simd_vector x)
@@ -182,60 +198,7 @@ static inline simd_vector simd_approx_exp(simd_vector x)
 #endif
 }
 
-//-----------------------------------------------------------------------------
-static inline simd_vector simd_log(simd_vector x)
-{
-    simd_vector one = simd_splat(1.f);
-    simd_vector invalid_mask = simd_cmp_le(x, simd_splat_zero());
-    x = simd_max(x, simd_min_normalized());  // cut off denormalized stuff
 
-#if defined(SIMD_NEON_IMPLEMENTATION)
-    int32x4_t emm0 = vshlq_s32(vreinterpretq_f32_s32(x), vdupq_n_s32(-23));
-    emm0 = vsubq_s32(emm0, vdupq_n_s32(0x7f));
-    simd_vector e = vcvtq_f32_s32(emm0);
-#elif defined(SIMD_AVX_IMPLEMENTATION)
-    __m256i emm0 = _mm256_srli_epi32(_mm256_castps_si256(x), 23);
-    emm0 = _mm256_sub_epi32(emm0, _mm256_set1_epi32(0x7f));
-    simd_vector e = _mm256_cvtepi32_ps(emm0);
-#endif
-    
-    // keep only the fractional part
-    x = simd_and(x, simd_inv_mant_mask());
-    x = simd_or(x, simd_splat(0.5f));
-    
-    e = simd_add(e, one);
-    simd_vector mask = simd_cmp_lt(x, simd_splat(0.707106781186547524f));
-    simd_vector tmp = simd_and(x, mask);
-    x = simd_sub(x, one);
-    e = simd_sub(e, simd_and(one, mask));
-    x = simd_add(x, tmp);
-
-    simd_vector z = simd_mul(x,x);
-    simd_vector y = simd_splat(7.0376836292E-2f);
-    y = simd_fmad(y, x, simd_splat(-1.1514610310E-1f));
-    y = simd_fmad(y, x, simd_splat(1.1676998740E-1f));
-    y = simd_fmad(y, x, simd_splat(-1.2420140846E-1f));
-    y = simd_fmad(y, x, simd_splat(+1.4249322787E-1f));
-    y = simd_fmad(y, x, simd_splat(-1.6668057665E-1f));
-    y = simd_fmad(y, x, simd_splat(+2.0000714765E-1f));
-    y = simd_fmad(y, x, simd_splat(-2.4999993993E-1f));
-    y = simd_fmad(y, x, simd_splat(+3.3333331174E-1f));
-    y = simd_mul(y, x);
-    y = simd_mul(y, z);
-
-    tmp = simd_mul(e, simd_splat(-2.12194440e-4f));
-    y = simd_add(y, tmp);
-
-    tmp = simd_mul(z, simd_splat(0.5f));
-    y = simd_sub(y, tmp);
-
-    tmp = simd_mul(e, simd_splat(0.693359375f));
-    x = simd_add(x, y);
-    x = simd_add(x, tmp);
-    x = simd_or(x, invalid_mask); // negative arg will be NAN
-
-    return x;
-}
 
 //-----------------------------------------------------------------------------
 // based on infinite binomial serie
