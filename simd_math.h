ca98b288c4e7c1077d32c78f863f91273556269f
Geolm <moyaguillaume@gmail.com>
2024-01-11 19:39:51 -0500

    updated comment

--- simd_math.h
+++ simd_math.h
@@ -20,7 +20,7 @@ void simd_sincos(simd_vector x, simd_vector* s, simd_vector* c); // outputs same
 simd_vector simd_acos(simd_vector x); // outputs same value than acosf()
 simd_vector simd_asin(simd_vector x); // outputs same value than asinf()
 simd_vector simd_atan(simd_vector x); // max error with input [-10; 10]: 0.000002
-simd_vector simd_atan2(simd_vector x, simd_vector y); // max error : 0.000001
+simd_vector simd_atan2(simd_vector x, simd_vector y); // max error : 2.38418579E-7
 simd_vector simd_log(simd_vector x); // outputs same value than logf()
 simd_vector simd_exp(simd_vector x); // outputs same value than expf()
 
